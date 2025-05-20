59352878a44eddcfec2a2af7f691257eb4e04034
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-05-20 12:19:55 +0330

    Update loss.py

--- utils/loss.py
+++ utils/loss.py
@@ -1,20 +1,27 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torch.utils
-
 
 class KDLoss(nn.Module):
-    def __init__(self):
+    def __init__(self, temperature=1.0):
         super(KDLoss, self).__init__()
+        self.temperature = temperature
 
     def forward(self, logits_t, logits_s):
+        # تبدیل logitها به احتمالات باینری
+        prob_t = torch.sigmoid(logits_t / self.temperature)
+        prob_s = torch.sigmoid(logits_s / self.temperature)
+        
+        # ساخت توزیع دوکلاسه: [p, 1-p]
+        prob_t_2d = torch.stack([1 - prob_t, prob_t], dim=-1)  # شکل: [batch_size, 2]
+        prob_s_2d = torch.stack([1 - prob_s, prob_s], dim=-1)  # شکل: [batch_size, 2]
+        
+  
         return F.kl_div(
-            F.log_softmax(logits_s, dim=1),
-            F.softmax(logits_t, dim=1),
-            reduction="batchmean",
-        )
-
+            F.log_softmax(prob_s_2d, dim=1),
+            prob_t_2d,
+            reduction='batchmean'
+        ) * (self.temperature ** 2)
 
 class RCLoss(nn.Module):
     def __init__(self):
@@ -27,14 +34,12 @@ class RCLoss(nn.Module):
     def forward(self, x, y):
         return (self.rc(x) - self.rc(y)).pow(2).mean()
 
-
 class MaskLoss(nn.Module):
     def __init__(self):
         super(MaskLoss, self).__init__()
 
     def forward(self, Flops, Flops_baseline, compress_rate):
         return torch.pow(Flops / Flops_baseline - compress_rate, 2)
-
 
 class CrossEntropyLabelSmooth(nn.Module):
     def __init__(self, num_classes, epsilon):
