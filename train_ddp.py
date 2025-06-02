f19d4b3d761830b1029a7beb9973ecdcc7785ad0
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-06-02 10:45:18 +0330

    Update train_ddp.py

--- train_ddp.py
+++ train_ddp.py
@@ -337,7 +337,7 @@ class TrainDDP:
 
         torch.cuda.empty_cache()
         self.teacher.eval()
-        scaler = GradScaler()
+        scaler = GradScaler('cuda')
 
         if self.resume:
             self.resume_student_ckpt()
@@ -382,7 +382,7 @@ class TrainDDP:
                             self.logger.warning("Invalid input detected (NaN or Inf)")
                         continue
 
-                    with autocast():
+                    with autocast('cuda'):
                         logits_student, feature_list_student = self.student(images)
                         logits_student = logits_student.squeeze(1)
                         with torch.no_grad():
