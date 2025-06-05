2eb6153cf8c36e621be14ec81dc828115fd55483
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-06-05 12:48:47 +0330

    Update ResNet_sparse.py

--- model/student/ResNet_sparse.py
+++ model/student/ResNet_sparse.py
@@ -36,8 +36,11 @@ class SoftMaskedConv2d(nn.Module):
         nn.init.kaiming_normal_(self.mask_weight)
 
     def compute_mask(self, ticket):
-        if torch.isnan(self.mask_weight).any() or torch.isinf(self.mask_weight).any():
-            print(f"NaN or Inf in mask_weight for layer {self.layer_name}")
+        if torch.isnan(self.mask_weight).any()
+            print(f"NaN  in mask_weight for layer {self.layer_name}")
+
+         if torch.isinf(self.mask_weight).any():
+             print(f"Inf in mask_weight for layer {self.layer_name}")
         
         if ticket:
             mask = torch.argmax(self.mask_weight, dim=1).float().view(-1, 1, 1, 1)
