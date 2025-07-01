fd2c638117aeff295b590b8cc12d3032248dc012
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-07-01 07:54:40 +0330

    Update transfer_learning.py

--- transfer_learning.py
+++ transfer_learning.py
@@ -3,8 +3,8 @@ import pandas as pd
 import torch
 import torch.nn as nn
 import torch.optim as optim
-from torch.utils.data import DataLoader, Subset, random_split
-from torchvision import transforms, models, datasets
+from torch.utils.data import DataLoader
+from torchvision import transforms, models
 from torch.amp import autocast, GradScaler
 from PIL import Image
 import argparse
@@ -13,106 +13,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from IPython.display import Image as IPImage, display
 from ptflops import get_model_complexity_info
-
-# تعریف Dataset_selector
-class Dataset_selector:
-    def __init__(self, dataset_mode, train_batch_size, eval_batch_size, num_workers, pin_memory, ddp, **kwargs):
-        self.dataset_mode = dataset_mode
-        self.transform = transforms.Compose([
-            transforms.Resize((160, 160) if dataset_mode == '125k' else (256, 256) if dataset_mode in ['rvf10k', '140k', '200k'] else (300, 300)),
-            transforms.ToTensor()
-        ])
-        
-        if dataset_mode == 'hardfake':
-            csv_file = kwargs.get('hardfake_csv_file')
-            root_dir = kwargs.get('hardfake_root_dir')
-            data = pd.read_csv(csv_file)
-            self.loader_train = DataLoader(
-                FaceDataset(data, root_dir, self.transform, split='train'),
-                batch_size=train_batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory
-            )
-            self.loader_val = DataLoader(
-                FaceDataset(data, root_dir, self.transform, split='val'),
-                batch_size=eval_batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory
-            )
-            self.loader_test = DataLoader(
-                FaceDataset(data, root_dir, self.transform, split='test'),
-                batch_size=eval_batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory
-            )
-        elif dataset_mode in ['rvf10k', '140k', '200k']:
-            train_csv = kwargs.get(f'realfake{dataset_mode}_train_csv')
-            val_csv = kwargs.get(f'realfake{dataset_mode}_val_csv')
-            test_csv = kwargs.get(f'realfake{dataset_mode}_test_csv')
-            root_dir = kwargs.get(f'realfake{dataset_mode}_root_dir')
-            train_data = pd.read_csv(train_csv)
-            val_data = pd.read_csv(val_csv)
-            test_data = pd.read_csv(test_csv) if test_csv else val_data
-            self.loader_train = DataLoader(
-                FaceDataset(train_data, root_dir, self.transform, split='train'),
-                batch_size=train_batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory
-            )
-            self.loader_val = DataLoader(
-                FaceDataset(val_data, root_dir, self.transform, split='val'),
-                batch_size=eval_batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory
-            )
-            self.loader_test = DataLoader(
-                FaceDataset(test_data, root_dir, self.transform, split='test'),
-                batch_size=eval_batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory
-            )
-        elif dataset_mode == '125k':
-            train_dataset = datasets.ImageFolder(os.path.join(kwargs.get('realfake125k_root_dir'), 'train'), transform=self.transform)
-            val_dataset = datasets.ImageFolder(os.path.join(kwargs.get('realfake125k_root_dir'), 'validation'), transform=self.transform)
-            
-            # تقسیم دیتاست validation به دو بخش: نصف برای validation و نصف برای تست
-            val_len = len(val_dataset)
-            val_subset_len = val_len // 2
-            test_subset_len = val_len - val_subset_len
-            val_subset, test_subset = random_split(val_dataset, [val_subset_len, test_subset_len])
-            
-            self.loader_train = DataLoader(
-                train_dataset, batch_size=train_batch_size, shuffle=True, num_workers=num_workers, pin_memory=pin_memory
-            )
-            self.loader_val = DataLoader(
-                val_subset, batch_size=eval_batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory
-            )
-            self.loader_test = DataLoader(
-                test_subset, batch_size=eval_batch_size, shuffle=False, num_workers=num_workers, pin_memory=pin_memory
-            )
-            # تنظیم DataFrame برای تست
-            self.loader_test.dataset.data = pd.DataFrame({
-                'filename': [os.path.join(*val_dataset.imgs[i][0].split(os.sep)[-3:]) for i in test_subset.indices],
-                'label': [val_dataset.imgs[i][1] for i in test_subset.indices]
-            })
-            # تنظیم DataFrame برای validation
-            self.loader_val.dataset.data = pd.DataFrame({
-                'filename': [os.path.join(*val_dataset.imgs[i][0].split(os.sep)[-3:]) for i in val_subset.indices],
-                'label': [val_dataset.imgs[i][1] for i in val_subset.indices]
-            })
-
-        # چاپ تعداد داده‌ها برای بررسی
-        print(f"Number of train samples for {dataset_mode}: {len(self.loader_train.dataset)}")
-        print(f"Number of validation samples for {dataset_mode}: {len(self.loader_val.dataset)}")
-        print(f"Number of test samples for {dataset_mode}: {len(self.loader_test.dataset)}")
-
-# تعریف FaceDataset برای دیتاست‌های CSV
-class FaceDataset:
-    def __init__(self, data, root_dir, transform=None, split='train'):
-        self.data = data
-        self.root_dir = root_dir
-        self.transform = transform
-        self.split = split
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        img_name = self.data.iloc[idx]['images_id' if self.split == 'hardfake' else 'filename']
-        label = self.data.iloc[idx]['label']
-        img_path = os.path.join(self.root_dir, img_name)
-        image = Image.open(img_path).convert('RGB')
-        if self.transform:
-            image = self.transform(image)
-        return image, label
+from data.dataset import FaceDataset, Dataset_selector
+from sklearn.model_selection import train_test_split
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Train a ResNet50 model with single output for fake vs real face classification.')
@@ -122,10 +24,10 @@ def parse_args():
                         help='Path to the dataset directory containing images and CSV file(s)')
     parser.add_argument('--teacher_dir', type=str, default='teacher_dir',
                         help='Directory to save the trained model and outputs')
-    parser.add_argument('--img_height', type=int, default=160,
-                        help='Height of input images (default: 160 for 125k, 300 for hardfake, 256 for rvf10k, 140k, 200k)')
-    parser.add_argument('--img_width', type=int, default=160,
-                        help='Width of input images (default: 160 for 125k, 300 for hardfake, 256 for rvf10k, 140k, 200k)')
+    parser.add_argument('--img_height', type=int, default=300,
+                        help='Height of input images (default: 300 for hardfake, 256 for rvf10k, 140k, 200k, 160 for 125k)')
+    parser.add_argument('--img_width', type=int, default=300,
+                        help='Width of input images (default: 300 for hardfake, 256 for rvf10k, 140k, 200k, 160 for 125k)')
     parser.add_argument('--batch_size', type=int, default=32,
                         help='Batch size for training')
     parser.add_argument('--epochs', type=int, default=15,
@@ -137,7 +39,7 @@ def parse_args():
 args = parse_args()
 
 os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-torch.backends.cudnn.benchmark = True
+torch.backends.cudnn.benchmark = True  
 torch.backends.cudnn.enabled = True
 
 dataset_mode = args.dataset_mode
@@ -169,9 +71,9 @@ if dataset_mode == 'hardfake':
 elif dataset_mode == 'rvf10k':
     dataset = Dataset_selector(
         dataset_mode='rvf10k',
-        realfake10k_train_csv=os.path.join(data_dir, 'train.csv'),
-        realfake10k_valid_csv=os.path.join(data_dir, 'valid.csv'),
-        realfake10k_root_dir=data_dir,
+        rvf10k_train_csv=os.path.join(data_dir, 'train.csv'),
+        rvf10k_valid_csv=os.path.join(data_dir, 'valid.csv'),
+        rvf10k_root_dir=data_dir,
         train_batch_size=batch_size,
         eval_batch_size=batch_size,
         num_workers=4,
@@ -205,11 +107,30 @@ elif dataset_mode == '200k':
         ddp=False
     )
 elif dataset_mode == '125k':
+    # Load valid.csv and split into validation and test
+    valid_csv_path = os.path.join(data_dir, 'valid.csv')
+    if not os.path.exists(valid_csv_path):
+        raise FileNotFoundError(f"Validation CSV file {ISIvalid_csv_path} not found!")
+    valid_data = pd.read_csv(valid_csv_path)
+    
+    # Split validation data into validation and test (50-50 split)
+    valid_data, test_data = train_test_split(valid_data, test_size=0.5, random_state=42, stratify=valid_data['label'])
+    
+    # Save temporary CSV files for validation and test
+    temp_valid_csv = os.path.join(teacher_dir, 'temp_valid_125k.csv')
+    temp_test_csv = os.path.join(teacher_dir, 'temp_test_125k.csv')
+    valid_data.to_csv(temp_valid_csv, index=False)
+    test_data.to_csv(temp_test_csv, index=False)
+    
     dataset = Dataset_selector(
         dataset_mode='125k',
+        realfake125k_train_csv=os.path.join(data_dir, 'train.csv'),
+        realfake125k_valid_csv=temp_valid_csv,
+        realfake125k_test_csv=temp_test_csv,
         realfake125k_root_dir=data_dir,
         train_batch_size=batch_size,
         eval_batch_size=batch_size,
+        test_batch_size=batch_size,
         num_workers=4,
         pin_memory=True,
         ddp=False
@@ -326,14 +247,14 @@ print(f'Test Loss: {test_loss / len(test_loader):.4f}, Test Accuracy: {100 * cor
 val_data = dataset.loader_test.dataset.data
 transform_test = dataset.loader_test.dataset.transform
 
-if dataset_mode == '140k':
+if dataset_mode == '140k' or dataset_mode == '125k':
     img_column = 'path'
-elif dataset_mode in ['200k', '125k']:
+elif dataset_mode == '200k':
     img_column = 'filename'
 else:
     img_column = 'images_id'
 
-if img_column not in val_data.columns and dataset_mode != '125k':
+if img_column not in val_data.columns:
     raise KeyError(f"Column '{img_column}' not found in DataFrame. Available columns: {list(val_data.columns)}")
 
 random_indices = random.sample(range(len(val_data)), min(10, len(val_data)))
@@ -345,12 +266,12 @@ with torch.no_grad():
         row = val_data.iloc[idx]
         img_name = row[img_column]
         label = row['label']
-        
-        if dataset_mode == '140k':
+       
+        if dataset_mode in ['140k', '125k']:
             img_path = os.path.join(data_dir, img_name)
-        elif dataset_mode in ['200k', '125k']:
-            subfolder = 'real' if label == 1 else 'fake'
-            img_path = os.path.join(data_dir, 'train' if 'train' in img_name else 'validation', subfolder, os.path.basename(img_name))
+        elif dataset_mode == '200k':
+            subfolder = 'real' if label == 1 else 'ai_images'
+            img_path = os.path.join(data_dir, 'my_real_vs_ai_dataset', 'my_real_vs_ai_dataset', subfolder, img_name)
         else:
             img_path = os.path.join(data_dir, img_name)
 
@@ -365,7 +286,7 @@ with torch.no_grad():
             output = model(image_transformed).squeeze(1)
         prob = torch.sigmoid(output).item()
         predicted_label = 'real' if prob > 0.5 else 'fake'
-        true_label = 'real' if label == 1 else 'fake'
+        mqtt = 'real' if label == 1 else 'fake'
         axes[i].imshow(image)
         axes[i].set_title(f'True: {true_label}\nPred: {predicted_label}', fontsize=10)
         axes[i].axis('off')
