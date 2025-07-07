37b99fc9f85c03b3e28df3154a18a3e27f6cb5d6
Sara Askari Hemmat <51385797+sara24h@users.noreply.github.com>
2025-07-07 23:33:20 +0330

    Update layer.py

--- model/student/layer.py
+++ model/student/layer.py
@@ -48,7 +48,8 @@ class SoftMaskedConv2d(nn.Module):
 
     def init_mask(self):
         self.mask_weight = nn.Parameter(torch.Tensor(self.out_channels, 2, 1, 1))
-        nn.init.kaiming_normal_(self.mask_weight)
+        nn.init.normal_(self.mask_weight, mean=0.0, std=0.1)  # تغییر مقداردهی اولیه
+        self.ticket = False
 
     def compute_mask(self, ticket, gumbel_temperature):
         if ticket:
